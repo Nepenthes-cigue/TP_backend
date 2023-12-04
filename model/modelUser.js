d39284c7d4fca2718266c1aa0ac6ac8eb3ad5eea
Nepenthes-cigue <emmarobert8322@gmail.com>
2023-12-04 09:40:08 +0100

    Creation user email unique

--- model/modelUser.js
+++ model/modelUser.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose
 
 const userSchema = new Schema({ //Number = int
     name: String, 
-    email: String,
+    email: {type: String, unique: true},
     password: String,
 })
 
